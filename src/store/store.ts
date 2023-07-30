7b54bccf38b147a51bd6940a9ad39c9958a3ca96
Saoa35 <saoamah@gmail.com>
2023-07-30 11:39:57 +0400

    + added bookings slice

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "src/store/user/slice";
 import tripReducer from "src/store/trips/slice";
+import bookingReducer from "src/store/bookings/slice";
 
 export const store = configureStore({
   reducer: {
     auth: userReducer,
     trips: tripReducer,
+    bookings: bookingReducer,
   },
 });
 
